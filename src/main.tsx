70d7d476c75b8bcb7dec451f5374a9f7ed5d5826
Milan <mmilanbel@gmail.com>
2024-07-10 05:51:42 +1000

    some change

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = '/tonconnect-manifest.json';
+const manifestUrl = 'https://my-twa-cyan.vercel.app/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
